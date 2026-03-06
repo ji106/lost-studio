be9c64aad342b1ab7cfe69c10cb1577489d80c5b
Jia <jiaxinji06@gmail.com>
2026-03-06 09:47:09 +0100

    Add comments to menu script functions

--- godot/scripts/menu.gd
+++ godot/scripts/menu.gd
@@ -1,19 +1,23 @@
 extends Control
 
 func _ready():
+	# Mostramos el cursor para que el jugador pueda interactuar con el menú
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_btn_new_game_pressed():
+	# Cambiamos el modo a "NEW" para crear una nueva partida y vamos al menú de guardados
 	Global.modo_menu = "NEW" 
 	TransitionScreen.cambiar_escena("res://tscn/menu_guardados.tscn")
 
 func _on_btn_load_game_pressed():
+	# Cambiamos el modo a "LOAD" para cargar una partida y vamos al menú de guardados
 	Global.modo_menu = "LOAD"
 	TransitionScreen.cambiar_escena("res://tscn/menu_guardados.tscn")
 
 func _on_btn_settings_pressed():
-	# ¡Ahora sí funciona!
+	# Abrimos la escena de configuración o ajustes
 	TransitionScreen.cambiar_escena("res://tscn/settings.tscn")
 
 func _on_btn_exit_pressed():
+	# Cerramos el juego
 	get_tree().quit()
